b08d4548c9aaf4cdd27b94e15e27b5e182b84a09
John Vogen <john.vogen@verizon.net>
2017-04-05 13:22:30 -0400

    Revert "change to production config file - TEST"
    This reverts commit d6492eddeacd9a45cdddd746fcd6b326b37987ea.

--- js/alpaca2.js
+++ js/alpaca2.js
@@ -33,21 +33,13 @@ function getPage(callback) {
     username = $("#txtUsername").val();
     password = $("#txtPassword").val();
 
-    var config = {
-                   "username": username,
-                 "password": password,
-                  "baseURL": "/proxy"
-
-     }
-
     //var config = {
-    //    "clientKey": "1bd1ddc4-37c7-4c80-b69b-b0d8d226cc34",
-    //    "clientSecret": "CamxJ6k/aNYbuZVV1uTox0imFpsURRugGjt/AD77DGENmJ+U87Z1eh4KBdKtCcY8/Regd9DH8DYWGJ2mcdSCsK3a+aX1WR2ftnxQQ8yg6ck=",
-    //    "username": username,
-    //    "password": password,
-    //    "baseURL": "https://api.cloudcms.com",
-    //    "application": "c8a4dc1dd5644f2934be"
-    //}
+    //               "username": username,
+    //             "password": password,
+    //              "baseURL": "/proxy"
+
+    // }
+
 
 
     if (!($("#txtUsername").val()) || (!($("#txtPassword").val()))) {
@@ -56,7 +48,14 @@ function getPage(callback) {
     }
     else {
 
-        
+        var config = {
+            "clientKey": "1bd1ddc4-37c7-4c80-b69b-b0d8d226cc34",
+            "clientSecret": "CamxJ6k/aNYbuZVV1uTox0imFpsURRugGjt/AD77DGENmJ+U87Z1eh4KBdKtCcY8/Regd9DH8DYWGJ2mcdSCsK3a+aX1WR2ftnxQQ8yg6ck=",
+            "username": username,
+            "password": password,
+            "baseURL": "https://api.cloudcms.com",
+            "application": "c8a4dc1dd5644f2934be"
+        }
 
         Gitana.connect(config, function (err) {
 
